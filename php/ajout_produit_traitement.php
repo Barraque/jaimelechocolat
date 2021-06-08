53532b990e77c651d320714b0d1f7c2db7f58e44
nigalex <nicolasbillereau@live.fr>
2021-06-08 08:01:12 +0200

    admin

---
+++ php/ajout_produit_traitement.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'config.php';
+
+if (!empty($_POST['nom']) && !empty($_POST['prix']) && !empty($_POST['marque']) && !empty($_POST['img_src']) && !empty($_POST['description'])) {
+    foreach ($_POST as $key => $value) {
+        $$key = htmlspecialchars($value);
+    }
+    $insert = $bdd->prepare('INSERT INTO produits ( nom, prix, marque, img_src, description) VALUES ( ?, ?, ?, ?, ?)');
+    $insert -> bindValue(1, $_POST['nom']);
+    $insert -> bindValue(2, $_POST['prix']);
+    $insert -> bindValue(3, $_POST['marque']);
+    $insert -> bindValue(4, $_POST['img_src']);
+    $insert -> bindValue(5, $_POST['description']);
+
+    $insert->execute();
+} else {
+    header('Location: admin.php?reg_err=password');
+    die();
+}
+
+?>
